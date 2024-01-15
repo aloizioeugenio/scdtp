f2e7170ea55b836a9252fca082b89426d928ec91
aloizioeugenio <aloizio.eugenio@gmail.com>
2024-01-15 16:37:39 -0300

    Commitando a tela_inicial do sistema

--- apps/tela_login/urls.py
+++ apps/tela_login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tela_login.views import funcao_login
+from tela_login.views import funcao_login, funcao_tela_inicial
 
 urlpatterns = [
-    path('', funcao_login)
+    path('', funcao_login),
+    path('tela_inicial/', funcao_tela_inicial, name='abre_tela_inicial')
 ]
--- apps/tela_login/views.py
+++ apps/tela_login/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
                    
 def funcao_login(request):
     return render(request, 'tela_login.html')
+
+def funcao_tela_inicial(request):
+    if request.method == 'GET':
+        return render(request, 'tela_inicial.html')
